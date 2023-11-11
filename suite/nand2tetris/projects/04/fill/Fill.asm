a566fa8538f278b204902d9bfbf0f4ec21065ec9
Grzegorz Bielski <pesiok@gmail.com>
2023-11-11 20:58:04 +0100

    fill and mult hack asm

--- suite/nand2tetris/projects/04/fill/Fill.asm
+++ suite/nand2tetris/projects/04/fill/Fill.asm
@@ -11,4 +11,73 @@
 // 'white' in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-//// Replace this comment with your code.
+(APP_LOOP)
+    @KBD
+    D=M
+    @FILL_BLACK
+    D;JNE
+    @FILL_WHITE
+    D;JEQ
+
+(FILL_BLACK)
+    @FILL_VALUE
+    M=-1
+    @FILL_SCREEN
+    0;JMP
+
+(FILL_WHITE)
+    @FILL_VALUE
+    M=0
+    @FILL_SCREEN
+    0;JMP
+
+(FILL_SCREEN)
+    // Screen is 32 16-bit words starting from @SCREEN
+    // 0 - white pixel
+    // 1 - black pixel
+
+    // R0 = array start address
+    @SCREEN
+    D=A
+    @R0
+    M=D
+    // R1 = array size
+    // 256 rows * 32 pixels/row = 8192 pixels
+    @8192
+    D=A
+    @R1
+    M=D
+    // generic array fill
+    @n
+    M=0
+(FILL_LOOP)
+    // if (n == R1) goto FILL_LOOP_END
+    @n
+    D=M
+    @R1
+    D=D-M
+    @FILL_LOOP_END
+    D;JEQ
+    // *(R0 + n) = @FILL_VALUE
+    @R0
+    D=M
+    @n
+    D=D+M
+    @FILL_PTR
+    M=D
+    @FILL_VALUE
+    D=M
+    @FILL_PTR
+    A=M
+    M=D
+    // n = n + 1
+    // @16
+    // D=A
+    @n
+    M=M+1
+    // goto FILL_LOOP
+    @FILL_LOOP
+    0;JMP
+(FILL_LOOP_END)
+    @APP_LOOP
+    0;JMP
--- suite/nand2tetris/projects/04/mult/mult.asm
+++ suite/nand2tetris/projects/04/mult/mult.asm
@@ -7,4 +7,51 @@
 // Assumes that R0 >= 0, R1 >= 0, and R0 * R1 < 32768.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-//// Replace this comment with your code.
+// R2 = R0 * R1
+
+// LOOP:
+//  if (R1 == 0 || R0 == 0) goto ASSIGN
+//  else 
+//     R1 = R1 - 1
+//     RESULT = RESULT + R0
+//     goto LOOP
+// ASSIGN: 
+//  R2 = RESULT
+//  goto END
+// END:
+
+@RESULT
+M=0
+
+(LOOP)
+    // if (R0 == 0) goto ASSIGN
+    @R0
+    D=M
+    @ASSIGN
+    D;JEQ
+    // if (R1 == 0) goto ASSIGN
+    @R1
+    D=M
+    @ASSIGN
+    D;JEQ
+    // R1 = R1 - 1
+    @R1
+    M=M-1
+    // RESULT = RESULT + R0
+    @R0
+    D=M
+    @RESULT
+    M=M+D
+    // goto LOOP
+    @LOOP
+    0;JMP
+(ASSIGN)
+    // R2 = RESULT
+    @RESULT
+    D=M
+    @R2
+    M=D
+(END)
+    // end loop
+    @END
+    0;JMP
